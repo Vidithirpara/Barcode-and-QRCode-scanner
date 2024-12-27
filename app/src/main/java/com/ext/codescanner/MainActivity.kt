b953010296de0982334aa2b019bf31dec86766a1
Vidithirpara <vidithirpara44@gmail.com>
2024-12-27 15:12:40 +0530

    Barcode and QRcode scanner

---
+++ app/src/main/java/com/ext/codescanner/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ext.codescanner
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ext.codescanner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.llOpenScanner.setOnClickListener {
+            val intent = Intent(this, BarcodeScannerActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
